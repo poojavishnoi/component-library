a48a33dad96d9551cc52c3b10e386c99e86f9b86
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-31 11:34:35 +0530

    Added validation in form

---
+++ components/Form/form.js
@@ -0,0 +1,11 @@
+const allForms = document.querySelectorAll(".submit-form-example");
+
+allForms.forEach((form)=>{
+    form.addEventListener("submit", (e)=>{
+        if(!form.checkValidity()){
+            e.preventDefault();
+        }
+        form.classList.add("form-validated");
+        
+    }, false)
+})
